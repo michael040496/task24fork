5c56d0995f15028428a43951161ea1e3ef1b05b8
Michael <Michael.Dejene.Kifle@no.experis.com>
2019-03-14 10:15:04 +0100

    first commit

---
+++ Task24.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const port = 8080
+
+let rawData = fs.readFileSync("data.json");
+const data = JSON.parse(rawData);
+
+app.get('/data', (req, res) => res.send(data));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.listen(process.env.PORT || 8080)
